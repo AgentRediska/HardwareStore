2e8f9e167bbc5a97f7c3189e5aa0c7d868437a07
AgentRediska <deniahalk98@mail.ru>
2022-02-15 20:40:38 +0300

    Customized keyboard in SearchFragment.kt

--- app/src/main/java/com/agentrediska/hardwarestore/presentation/searchfragment/SearchFragment.kt
+++ app/src/main/java/com/agentrediska/hardwarestore/presentation/searchfragment/SearchFragment.kt
@@ -2,12 +2,17 @@ package com.agentrediska.hardwarestore.presentation.searchfragment
 
 import android.content.Context
 import android.os.Bundle
+import android.view.KeyEvent
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
 import com.agentrediska.hardwarestore.R
 import com.agentrediska.hardwarestore.databinding.FragmentSearchBinding
+import android.widget.Toast
+
+
+
 
 
 class SearchFragment : Fragment(R.layout.fragment_search) {
@@ -19,11 +24,23 @@ class SearchFragment : Fragment(R.layout.fragment_search) {
         super.onViewCreated(view, savedInstanceState)
         _binding = FragmentSearchBinding.bind( view)
 
+        binding.searchEditText.setOnKeyListener(object : View.OnKeyListener {
+            override fun onKey(v: View?, keyCode: Int, event: KeyEvent): Boolean {
+                if (event.action == KeyEvent.ACTION_DOWN) {
+                    // Perform action on key press
+                    Toast.makeText(requireContext(), "dwdw", Toast.LENGTH_SHORT)
+                        .show()
+                    return true
+                }
+                return false
+            }
+        })
+
         binding.searchEditText.requestFocus()
         showSoftKeyboard(binding.searchEditText)
     }
 
-    fun showSoftKeyboard(view: View) {
+   private fun showSoftKeyboard(view: View) {
         if(view.requestFocus()){
             val imm: InputMethodManager = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
             imm.showSoftInput(view, InputMethodManager.SHOW_IMPLICIT)
